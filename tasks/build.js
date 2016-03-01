2e36ccfa799552ca3e12e3690d137136fbd6e913
Michael Rose <michael_rose@outlook.com>
2016-02-29 22:56:11 -0500

    Remove copy logs task

--- tasks/build.js
+++ tasks/build.js
@@ -7,14 +7,9 @@ import cssnano from 'gulp-cssnano';
 import htmlmin from 'gulp-htmlmin';
 import runSequence from 'run-sequence';
 
-gulp.task('copy:logs', () => {
-  gulp.src('app/public/logs/**')
-    .pipe(gulp.dest('app/dist/logs'));
-});
-
 // build for production: concatenate, minify
 gulp.task('build', (done) => {
-  runSequence('clean', ['compile', 'copy:logs'], () => {
+  runSequence('clean', 'compile', () => {
     gulp.src(['app/public/*[!lib]*/*.html', 'app/public/*.html'])
       .pipe(useref())
       .pipe(gulpif(['*.js', '!vendor/*.js'], uglify()))
