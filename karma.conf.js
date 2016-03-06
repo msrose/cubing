521134783d206ec15d4deb292bf07a41c1de9537
Michael Rose <michael_rose@outlook.com>
2016-03-05 21:08:27 -0500

    Fix sample test running

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/public/lib/jquery/dist/jquery.min.js',
-      'app/public/lib/babel-polyfill/polyfill.min.js',
-      'app/public/scripts/**/*.js',
       'app/public/tests/**/*.js'
     ],
 
